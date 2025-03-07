bf52f00214b54e2cbbd69b078188ada61d33ee18
brunobermudez18 <110926605+brunobermudez18@users.noreply.github.com>
2025-03-06 19:19:08 -0600

    rubix

--- motoman.py
+++ motoman.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from roboticstoolbox import DHRobot, RevoluteDH
 from spatialmath import SE3
 from roboticstoolbox.tools.trajectory import mstraj
+from codigoVerTrayect import plot_robot_trajectory
 
 # Convert degrees to radians
 deg_to_rad = np.pi / 180
@@ -22,24 +23,24 @@ mh5.tool = SE3.OA([0, -1, 0], [1, 0, 0])  # X-axis forward, Y-axis backward
 
 # Define the full cube trajectory (all 12 edges)
 T = np.array([
-    [ 0.55,  0.35,  0.15],  # A (Start)
-    [ 0.55, -0.20,  0.15],  # B
-    [ 0.70, -0.20,  0.15],  # C
-    [ 0.70,  0.35,  0.15],  # D
-    [ 0.55,  0.35,  0.15],  # A (Close Bottom Face)
+    [0.55,  0.00,  0.20],  # A (Start)
+    [0.55,  0.15,  0.20],  # B
+    [0.70,  0.15,  0.20],  # C
+    [0.70,  0.00,  0.20],  # D
+    [0.55,  0.00,  0.20],  # A (Close Bottom Face)
 
-    [ 0.55,  0.35,  0.40],  # E (Move up)
-    [ 0.55, -0.20,  0.40],  # F
-    [ 0.70, -0.20,  0.40],  # G
-    [ 0.70,  0.35,  0.40],  # H
-    [ 0.55,  0.35,  0.40],  # E (Close Top Face)
+    [0.55,  0.00,  0.35],  # E (Move up)
+    [0.55,  0.15,  0.35],  # F
+    [0.70,  0.15,  0.35],  # G
+    [0.70,  0.00,  0.35],  # H
+    [0.55,  0.00,  0.35],  # E (Close Top Face)
 
-    [ 0.55, -0.20,  0.40],  # F (Connect top to bottom)
-    [ 0.55, -0.20,  0.15],  # B
-    [ 0.70, -0.20,  0.15],  # C
-    [ 0.70, -0.20,  0.40],  # G
-    [ 0.70,  0.35,  0.40],  # H
-    [ 0.70,  0.35,  0.15],  # D
+    [0.55,  0.15,  0.35],  # F (Connect top to bottom)
+    [0.55,  0.15,  0.20],  # B
+    [0.70,  0.15,  0.20],  # C
+    [0.70,  0.15,  0.35],  # G
+    [0.70,  0.00,  0.35],  # H
+    [0.70,  0.00,  0.20]   # D
 ])
 
 # Generate a smooth trajectory using mstraj
@@ -63,6 +64,44 @@ sol = mh5.ikine_LM(T_tool, q0= [0, 0, 0, 0, 0, 0], mask=[1, 1, 1, 0, 0, 0])
 # Check if solution is valid
 if sol.success:
     print("IK solution found!")
-    mh5.plot(q=sol.q, limits=[-0.3, 0.8, -0.6, 0.8, -0.1, 1], eeframe=True, shadow=True, jointaxes=False, block=True)
+    #mh5.plot(q=sol.q, limits=[-0.3, 0.8, -0.6, 0.8, -0.1, 1], eeframe=True, shadow=True, jointaxes=False, block=True)
+    p_lim=[-1, 1, -1, 1, -0.15, 1.5]
+    plot_robot_trajectory(
+        robot=mh5,
+        q_trajectory=sol.q,
+        limits=p_lim,
+        eeframe=True,
+        jointaxes=False,
+        shadow=True,
+        drawing_mode='continuous',  # o 'segments' si prefieres
+        traj_color='r',             # Color de la trayectoria completa
+        drawing_color='b',          # Color del trazo principal
+        dt=0.05,
+        block=True
+    )
 else:
-    print("No valid IK solution found!")
+    print("Fuck off!")
+    
+T_tool2 = SE3(-0.15, 0, 0) 
+T_cubo  = [T_tool2 * SE3(p[0], p[1], p[2]) for p in T]
+    
+sol2 = mh5.ikine_LM(SE3(T_cubo), q0= [0, 0, 0, 0, np.deg2rad(-80), 0], mask=[1, 1, 1, 0, 0, 0], ilimit = 3000, slimit = 10, tol = 0.000000000001)
+if sol2.success:
+    print("IK solution found!")
+    #mh5.plot(q=sol2.q, limits=[-0.3, 0.8, -0.6, 0.8, -0.1, 1], eeframe=True, shadow=True, jointaxes=False, block=True, dt = 0.25)
+    p_lim=[-1, 1, -1, 1, -0.15, 1.5]
+    plot_robot_trajectory(
+        robot=mh5,
+        q_trajectory=sol2.q,
+        limits=p_lim,
+        eeframe=True,
+        jointaxes=False,
+        shadow=True,
+        drawing_mode='continuous',  # o 'segments' si prefieres
+        traj_color='r',             # Color de la trayectoria completa
+        drawing_color='b',          # Color del trazo principal
+        dt=0.25,
+        block=True
+    )
+else:
+    print("Fuck off") 
